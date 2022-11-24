9c059e0ec975ecdbfd4551033c120960991f973e
Aleksandr Terekhov <a_terexov@mail.ru>
2022-11-24 20:12:43 +0500

    addewd cycle

--- print.py
+++ print.py
@@ -1,4 +1,8 @@
 print('Сколько можно это не понимать, твою мать!!!!!')
-number = int(input('Введите 1 число'))
-
-
+number = int(input('Введите 1 число: '))
+if number > 0:
+    print(f'Число {number} положительное')
+elif number < 0:
+    print(f'Число {number} отрицательное')
+else:
+    print('Не то не се!')
